80edd6089e3cda83c09f0cf807e4a144a7ba12e9
Meghal <meghal.shah@bacancy.com>
2025-09-12 23:26:38 +0530

    intial commit

---
+++ Budgetly.API/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Budgetly.Application;
+using Budgetly.Infrastructure;
+
+namespace Budgetly.API
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddAppDI(this IServiceCollection services)
+        {
+            services.AddApplicationDI();
+            services.AddInfrastructureDI();
+
+            return services;
+        }
+    }
+}
---
+++ Budgetly.Application/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Budgetly.Core;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Budgetly.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationDI(this IServiceCollection services)
+        {
+            services.AddCoreDI();
+
+            return services;
+        }
+    }
+}
