5c13d8815edf7fef0266deb668ba82a2bf20a0ce
meghal-bacancy <meghal.shah@bacancy.com>
2025-10-08 09:54:30 +0530

    .angular removed

--- Budgetly.Client/src/app/shared/shared.module.ts
+++ Budgetly.Client/src/app/shared/shared.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PrimaryHeaderComponent } from './components/primary-header/primary-header.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
     declarations: [
         PrimaryHeaderComponent,
-        PageNotFoundComponent
+        PageNotFoundComponent,
+        DashboardComponent
     ],
     imports: [
         CommonModule
